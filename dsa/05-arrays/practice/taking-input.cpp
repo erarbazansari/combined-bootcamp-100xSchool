77c879f0ef72f80e3a58128dda09995924429946
Arbaz Ansari <mail.arbazansari@gmail.com>
2026-02-06 22:42:42 +0530

    Add initial implementation for appointment booking system
    - Created a basic Express server with authentication routes for user registration and login.
    - Implemented user model and Prisma configuration for PostgreSQL database.
    - Added validation schemas for user input during registration and login.
    - Included middleware for JWT authentication.
    - Set up project structure with necessary files for development and deployment.

---
+++ dsa/05-arrays/practice/taking-input.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int arr[10];
+    for (int i = 0; i < 10; i++)
+    {
+        cin >> arr[i];
+    }
+    // printing
+    for (int i = 0; i < 10; i++)
+    {
+        cout << arr[i];
+    }
+    cout << endl;
+    return 0;
+}
